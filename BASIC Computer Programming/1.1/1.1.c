431f67b3274d907ec4f2c624eccd6adeaf7d5803
นรบดินทร์ นุชทรวง <guitar444888@gmail.com>
2025-11-19 20:51:57 +0700

    Add files via upload

--- BASIC Computer Programming/1.1/1.1.c
+++ BASIC Computer Programming/1.1/1.1.c
@@ -7,16 +7,16 @@ int main()
     {
         return 1;
     }
-    if (N <= 0)
-    {
-        printf("Error\n");
-    }
-    else
+    if (N > 0)
     {
         for (i = 0; i < N; i++)
         {
             printf("Hello Loop!\n");
         }
+    }
+    else
+    {
+        printf("Error\n");
     }
     return 0;
 }
