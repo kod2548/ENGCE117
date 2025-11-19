02fecb3c39e43b115b81966b76cde3f35f97412e
นรบดินทร์ นุชทรวง <guitar444888@gmail.com>
2025-11-19 19:19:44 +0700

    Add files via upload

---
+++ BASIC Computer Programming/1.1/1.1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int N, i;
+    if (scanf("%d", &N) != 1)
+    {
+        return 1;
+    }
+    if (N <= 0)
+    {
+        printf("The number is incorrect\n");
+    }
+    for (i = 0; i < N; i++)
+    {
+        printf("Hello Loop!\n");
+    }
+    return 0;
+}
