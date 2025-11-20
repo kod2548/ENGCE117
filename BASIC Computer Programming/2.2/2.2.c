9da75d2ae80ebbe26282f736ae5822728d6a7106
นรบดินทร์ นุชทรวง <guitar444888@gmail.com>
2025-11-20 15:53:39 +0700

    Add files via upload

---
+++ BASIC Computer Programming/2.2/2.2.c
@@ -0,0 +1,41 @@
+// Pre-code (โค้ดบังคับ)
+#include <stdio.h>
+
+int main()
+{
+    int N, i, quantity;
+    float unitPrice, itemCost, grandTotal = 0.0;
+
+    // รับค่า N (จำนวนรายการ)
+    if (scanf("%d", &N) != 1)
+    {
+        return 1;
+    }
+
+    // TODO: Implement the for loop (N times)
+    // TODO: Inside the loop, receive unitPrice and quantity
+    // TODO: Implement the if-else discount logic and accumulate grandTotal
+    for (i = 0; i < N; i++)
+    {
+        if (scanf("%f", &unitPrice) != 1)
+        {
+            return 1;
+        }
+        if (scanf("%d", &quantity) != 1)
+        {
+            return 1;
+        }
+
+        itemCost = unitPrice * quantity;
+
+        if (unitPrice >= 1000)
+        {
+            itemCost = itemCost * 0.90;
+        }
+
+        grandTotal += itemCost;
+    }
+
+    printf("Grand Total: %.2f\n", grandTotal);
+    return 0;
+}
