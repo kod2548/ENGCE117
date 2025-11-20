bdebf5dc198e2be8666cf79fe2acc95f1af326f4
นรบดินทร์ นุชทรวง <guitar444888@gmail.com>
2025-11-20 12:54:10 +0700

    Add files via upload

--- BASIC Computer Programming/1.4/1.4.c
+++ BASIC Computer Programming/1.4/1.4.c
@@ -1,14 +1,17 @@
+// Pre-code (โค้ดบังคับ)
 #include <stdio.h>
 
 int main()
 {
     int N, sum = 0, i;
 
+    // รับค่า N
     if (scanf("%d", &N) != 1)
     {
-        return 1;
+        return 1; // Handle input failure
     }
 
+    // TODO: Implement the for loop to calculate the sum from 1 to N
     for (i = 0; i <= N; i++)
     {
         sum += i;
