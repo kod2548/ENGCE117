e4cf355255779cb1e544b2166d8a4aae722d3b6e
นรบดินทร์ นุชทรวง <guitar444888@gmail.com>
2025-11-23 21:39:54 +0700

    Add files via upload

---
+++ BASIC Computer Programming/4.7/4.7.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+    const int MAX_SIZE = 10;
+    int data[MAX_SIZE];
+    int N, i;
+    int search_value;
+    int count = 0;
+
+    printf("Enter the number of elements (N max %d): ", MAX_SIZE);
+    scanf("%d", &N);
+
+    if (N > MAX_SIZE || N < 1)
+    {
+        N = MAX_SIZE;
+    }
+
+    printf("Enter %d integer numbers:\n", N);
+    for (i = 0; i < N; i++)
+    {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &data[i]);
+    }
+
+    printf("Enter the Search Value: ");
+    scanf("%d", &search_value);
+
+    printf("\n--- FREQUENCY ANALYSIS REPORT ---\n");
+    printf("Total elements recorded (N): %d\n", N);
+    printf("Recorded Numbers: ");
+    for (i = 0; i < N; i++)
+    {
+        printf("%d ", data[i]);
+        if (data[i] == search_value)
+        {
+            count++;
+        }
+    }
+    printf("\n");
+    printf("Search Value: %d\n", search_value);
+    printf("Frequency Count: %d\n", count);
+
+    return 0;
+}
