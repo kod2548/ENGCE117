e312c9faf36e7651e645649b2653323d8ac5c6b3
นรบดินทร์ นุชทรวง <guitar444888@gmail.com>
2025-11-25 21:54:58 +0700

    Add files via upload

---
+++ BASIC Computer Programming/6.1/6.1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void increase_value(int *number_ptr)
+{
+    *number_ptr += 10;
+}
+
+int main()
+{
+    int data;
+
+    printf("Enter initial integer value: ");
+    scanf("%d", &data);
+
+    printf("\n--- POINTER MODIFICATIOB REPORT ---\n");
+    printf("1. Value BEFORE function call: %d\n", data);
+
+    increase_value(&data);
+
+    printf("2. Value AFTER function call: %d\n", data);
+    return 0;
+}
