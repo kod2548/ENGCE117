c892febbaaefe88187e40dd8ef79906906b172d1
นรบดินทร์ นุชทรวง <guitar444888@gmail.com>
2025-11-26 23:22:21 +0700

    Add files via upload

---
+++ ENGCE117/LAB_1/1_2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void GetSet(int array[], int *number)
+{
+    int i;
+
+    printf("Enter number of members: ");
+    scanf("%d", number);
+
+    for (i = 0; i < *number; i++)
+    {
+        printf("Enter Member [%d]: ", i + 1);
+        scanf("%d", &array[i]);
+    }
+}
+
+int main()
+{
+    int i, n = 100;
+    int data[n], num;
+    GetSet(data, &num);
+    n = num;
+
+    printf("\n----- Member Report -----\n");
+    printf("Member have: %d\n", num);
+    for (i = 0; i < num; i++)
+    {
+        printf("Member [%d]: %d\n", i + 1, data[i]);
+    }
+
+    return 0;
+}
